a82a23b3156141c9089d8d985fa94ae7f174271b
Swapnil Jain <jainswapnil138@gmail.com>
2024-11-25 08:11:21 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css'; //good practice to import global styles here
+import { inter } from '@/app/ui/fonts';
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
